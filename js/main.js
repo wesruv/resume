45c58cac167a6a9f779744e3ebb76264c2abb5ab
Wes Ruvalcaba <wruvalcaba@giantrabbit.com>
2026-04-03 17:11:32 -0500

    Abbreviating work experience and updating some design and interaction

--- js/main.js
+++ js/main.js
@@ -1,9 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-  // Get scrollbar width and set it to CSS var
-  const $body = document.querySelector('body');
-  const scrollBarWidth = window.innerWidth - $body.offsetWidth;
-  $body.style.setProperty('--scrollBarWidth', `${scrollBarWidth}px`);
-
   // Add class to header when we pass the first heading
   const $employmentHeader = document.querySelector('#experience-list__heading--employment');
   const $header = document.querySelector('.resume-wrapper__header');
@@ -15,4 +10,19 @@ document.addEventListener('DOMContentLoaded', () => {
   );
 
   observer.observe($employmentHeader);
+
+  // Abbreviate work history
+  $moreButton = document.querySelector('.experience-list__more');
+  $moreButton.setAttribute('aria-expanded', 'false');
+
+  $moreButton.addEventListener('click', () => {
+    const toggleClass = 'experience-list__more--pressed';
+    $moreButton.classList.toggle(toggleClass);
+    if ($moreButton.classList.contains(toggleClass)) {
+      $moreButton.setAttribute('aria-expanded', 'true');
+    }
+    else {
+      $moreButton.setAttribute('aria-expanded', 'false');
+    }
+  });
 });
